d722bc1f9fb789590dc502634de7758fc9abff0f
Mateus-Mont <mateusmonteiropereira2@gmail.com>
2023-06-06 13:38:40 -0300

    feat: style button home page

--- src/components/button/style.ts
+++ src/components/button/style.ts
@@ -4,9 +4,33 @@ import { iStyledButtonProps } from "./type";
 export const StyledButton = styled.button<iStyledButtonProps>`
   ${({ buttonVariation }) => {
     switch (buttonVariation) {
-      case "login":
+      case "register":
         return css`
-          //Estilização aqui
+          padding: 10px 20px;
+          font-size: 16px;
+          border: 2px solid var(--color-grey4);
+          border-radius: var(--radius-2);
+          background-color: var(--color-grey10);
+          font-weight: var(--text-weight600);
+        `;
+      case "filters":
+        return css`
+          padding: 12px 28px 12px 28px;
+          font-size: 16px;
+          border-radius: var(--radius-2);
+          background-color: var(--color-brand2);
+          color: var(--color-white-fixed);
+          font-weight: var(--text-weight600);
+        `;
+      case "filterMaxAndMin":
+        return css`
+          width: 125px;
+          height: 37px;
+          font-size: 16px;
+          font-family: "Lexend";
+          background-color: var(--color-grey5);
+          color: var(--color-grey3);
+          font-weight: var(--text-weight600);
         `;
     }
   }}
