a060ef29363406653a5aaef999f5fbaf37c7ef21
Mateus-Mont <mateusmonteiropereira2@gmail.com>
2023-06-14 17:30:46 -0300

    feat: user authentication, toastfy and login logic

--- src/context/userContext/types.ts
+++ src/context/userContext/types.ts
@@ -1,3 +1,4 @@
+import React, { SetStateAction } from "react";
 import { NavigateFunction } from "react-router-dom";
 
 export interface iUserContextProps {
@@ -5,4 +6,12 @@ export interface iUserContextProps {
 }
 export interface iUserContext {
   navigate: NavigateFunction;
+  submitLogin: (formData: iLoginData) => void;
+  unauthorized: string;
+  setUnauthorized: React.Dispatch<SetStateAction<string>>;
+}
+
+export interface iLoginData {
+  email: string;
+  password: string;
 }
--- src/context/userContext/userContext.tsx
+++ src/context/userContext/userContext.tsx
@@ -1,13 +1,46 @@
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { iUserContext, iUserContextProps } from "./types";
+import { iLoginData, iUserContext, iUserContextProps } from "./types";
+import { api } from "../../services/api";
+import { AxiosError } from "axios";
+import { toast } from "react-toastify";
 
 export const UserContext = createContext({} as iUserContext);
 
 export const UserProvider = ({ children }: iUserContextProps) => {
   const navigate = useNavigate();
+  const [unauthorized, setUnauthorized] = useState("");
+
+  const submitLogin = async (formData: iLoginData) => {
+    try {
+      const response = await api.post("/login", formData);
+      const { token } = response.data;
+
+      localStorage.setItem("@TOKEN", JSON.stringify(token));
+      toast.success("Entrando...");
+
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+
+    } catch (error) {
+      const currentError = error as AxiosError;
+
+      if (currentError.message === "Request failed with status code 401") {
+        setUnauthorized("Usuário ou senha inválido");
+      }
+
+      if (currentError.message === "Network Error") {
+        toast.error("Network Error");
+      }
+    }
+  };
 
   return (
-    <UserContext.Provider value={{ navigate }}>{children}</UserContext.Provider>
+    <UserContext.Provider
+      value={{ navigate, submitLogin, unauthorized, setUnauthorized }}
+    >
+      {children}
+    </UserContext.Provider>
   );
 };
