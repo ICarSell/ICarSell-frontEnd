bc0f85cd17f529ea5a133003085d6bcc8e6568ef
DiegoAndre Leffa <diegodocanto@gmail.com>
2023-06-15 20:29:48 -0300

    refactor: cardPriceFipe

--- src/pages/perfilPage/style.ts
+++ src/pages/perfilPage/style.ts
@@ -8,16 +8,16 @@ export const PerfilPageStyle = styled.div`
   div:nth-child(1) {
     height: 20%;
     width: 100%;
-    background-color: var(--color-brand1);
+    // background-color: var(--color-brand1);
   }
   div:nth-child(2) {
     height: 80%;
     width: 100%;
-    background-color: var(--color-grey8);
+    // background-color: var(--color-grey8);
   }
   div:nth-child(3) {
     position: absolute;
-    background-color: var(--color-white-fixed);
+    // background-color: var(--color-white-fixed);
     width: 80%;
     top: 25%;
     left: 50%;
@@ -25,7 +25,7 @@ export const PerfilPageStyle = styled.div`
   }
   div:nth-child(4) {
     position: absolute;
-    background-color: red;
+    // background-color: red;
     width: 90%;
     top: calc(35% + 30vh);
     left: 50%;
@@ -49,15 +49,15 @@ export const PerfilPageStyle = styled.div`
       }
     }
     div:nth-child(2) {
-      background-color: var(--color-white-fixed);
+      // background-color: var(--color-white-fixed);
       display: flex;
       align-items: center;
       gap: 10px;
       height: 30px;
       p {
         padding: 10px;
-        background-color: var(--color-brand3);
-        color: var(--color-brand2);
+        // background-color: var(--color-brand3);
+        // color: var(--color-brand2);
         border-radius: 8px;
         max-width: 100%;
       }
