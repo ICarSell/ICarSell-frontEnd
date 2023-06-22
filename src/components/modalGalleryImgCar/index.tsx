6078e343c1af38098daeb0e47458ecc3f227ad93
Mateus-Mont <mateusmonteiropereira2@gmail.com>
2023-06-22 11:19:37 -0300

    feat: modal gallery car

---
+++ src/components/modalGalleryImgCar/index.tsx
@@ -0,0 +1,29 @@
+import { Button } from "../button";
+import { ContainerTopDisplayedModal } from "../containerTopDisplayedModal";
+import { HeaderModal } from "../headerModal";
+
+export const ModalGalleryImgCar = ({
+  galleryImg,
+  setOpenModalGallery,
+}: any) => {
+  return (
+    <ContainerTopDisplayedModal>
+      <HeaderModal>
+        <h4 className="titleModal">Imagen do Veículo</h4>
+        <Button
+          type="button"
+          buttonVariation="closeModalHeader"
+          onClick={() => setOpenModalGallery(false)}
+        >
+          X
+        </Button>
+      </HeaderModal>
+      <div className="containerImgGallery">
+        <img
+          src={`http://localhost:3000/${galleryImg?.path.replace(/\\/g, "/")}`}
+          alt={galleryImg?.fileName}
+        />
+      </div>
+    </ContainerTopDisplayedModal>
+  );
+};
