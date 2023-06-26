2f4f17379e49993068d6e96984ed28fd254e5196
Mateus-Mont <mateusmonteiropereira2@gmail.com>
2023-06-26 11:26:55 -0300

    feat: empty list component

---
+++ src/components/emptyList/index.tsx
@@ -0,0 +1,5 @@
+import { StyledTitleListEmpty } from "./style";
+
+export const EmptyList = ({ children }: { children: React.ReactNode }) => {
+  return <StyledTitleListEmpty>{children}</StyledTitleListEmpty>;
+};
